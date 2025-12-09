34f96f41b1c5db98459799f7d8ba0bc4ce81f5b0
jamesz <person119iscool@gmail.com>
2025-12-08 20:49:17 -0800

    Added every file... again...

---
+++ node_2d.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+@onready var timer_2: Timer = $Timer2
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_2_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
