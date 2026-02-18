b37eea131f10579746dd4f849c18b551ea7ee8cd
James Zheng <james.zheng@surreyschools.ca>
2026-02-18 15:40:10 -0800

    added files

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,21 +1,30 @@
 extends CharacterBody2D
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 200.0
 
-func _physics_process(delta: float) -> void:
-
+func _process(delta: float) -> void:
 	var direction_x := Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
-	
 	if direction_x:
 		velocity.x = direction_x * SPEED
+		if direction_x == -1:
+			sprite.play("left")
+		else:
+			sprite.play("right")
+
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	if direction_y:
 		velocity.y = direction_y * SPEED
+		if direction_y == -1:
+			sprite.play("back")
+		else:
+			sprite.play("front")
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
